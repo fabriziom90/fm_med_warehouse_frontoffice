f982c990a5a2584838d45cf98b336c072dfca7c9
fabriziom90 <fabriziomastrobattista@gmail.com>
2026-01-22 12:11:36 +0100

    to-delete

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {router} from '../router';
 
 // create custom axios api
+console.log(import.meta.env.VITE_API_BASE_URL);
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
     headers: {
